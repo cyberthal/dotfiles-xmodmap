1ec858dcd3cc35be65c677b0a65f38cdb953ef34
Texas Cyberthal <texas.cyberthal@gmail.com>
2017-01-18 11:16:59 +0800

    make deploy script path agnostic

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 rm -f ~/.Xmodmap
-ln -s ~/1-mansort/git-/hub/dotfiles/dotfiles-xmodmap/.Xmodmap ~/
+ln -s $SCRIPT_DIR/.Xmodmap ~/
 xmodmap ~/.Xmodmap
